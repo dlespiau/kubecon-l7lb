ba873d163d8b3c9f8678b0536fb4e1d32f78386a
Damien Lespiau <damien@weave.works>
2018-04-18 15:23:46 +0100

    service: Model requestion duration
    We now have a couple of choices to model request duration:
    
      - constant duration
      - duration following a log-normal distribution
    
    Updates: #3

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -7,7 +7,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
+
+var service struct {
+	duration Rander
+}
 
 func ready(w http.ResponseWriter, r *http.Request) {
 }
@@ -27,11 +32,27 @@ func hostname(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	io.WriteString(w, string(body))
+	time.Sleep(time.Duration(service.duration.Rand() * float64(time.Millisecond)))
 }
 
 func main() {
 	listen := flag.String("listen", ":8080", "address the service will listen on")
+	durationModel := flag.String("duration.model", "log-normal", "enable request duration modelling")
+	constant := flag.Float64("duration.constant.ms", 10, "time (in ms) of request duration")
+	mu := flag.Float64("duration.log-normal.mu", 3, "μ parameter of the lognormal distribution of request duration")
+	sigma := flag.Float64("duration.log-normal.sigma", 0.4, "σ parameter of the lognormal distribution of request duration")
 	flag.Parse()
+
+	switch *durationModel {
+	case "zero":
+		service.duration = &Constant{0}
+	case "constant":
+		service.duration = &Constant{*constant}
+	case "log-normal":
+		service.duration = &LogNormal{Mu: *mu, Sigma: *sigma}
+	default:
+		log.Fatalf("unknown duration model: %s", *durationModel)
+	}
 
 	http.HandleFunc("/ready", ready)
 	http.HandleFunc("/echo", echo)
